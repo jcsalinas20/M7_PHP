381a816b3a163cbadceb8ea6004ba8713e574b33
jcsalinas20 <jcsalinasn20@gmail.com>
2020-10-07 22:47:44 +0200

    ocultar los dias que no son del mes actual

--- Calendar.php
+++ Calendar.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $NAME ?></title>
     <style>
+        .hidden {
+            visibility: hidden;
+        }
         h1 {
             text-align: center;
         }
@@ -20,6 +23,7 @@
         }
         td.value {
             height: 80px;
+            width: 100%;
             display: flex;
             padding: 0;
         }
@@ -47,42 +51,54 @@
     <h1><?php echo date("F") . " - " . date("Y")  ?></h1>
     <table>
         <?php
-        
-            $first_day = strftime("%w", mktime(0, 0, 0, 1, 1, $year));
 
-            $numDay = 3;
-            $namesDays = ["D", "L", "M", "X", "J", "V", "S"];
+            $month = date("m"); $year = date("Y"); // mes y año actual
+
+            $numDay = strftime("%w", mktime(0,0,0,$month, 1, $year));
+            $namesDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // Nombres de los dias
 
             echo "<tr>";
-            for ($i=0; $i < sizeof($namesDays); $i++) echo "<td class='days'>$namesDays[$i]</td>";
+            for ($i=0; $i < sizeof($namesDays); $i++) echo "<td class='days'>$namesDays[$i]</td>"; // Mostrar en la tabla los nombres de los dias
             echo "</tr>";
 
-            $numDias = cal_days_in_month(CAL_GREGORIAN, date("m"), date("Y"));
-            $contador = 1;
-            $semanas = ceil(($numDias + $numDay) / 7);
+            $numDias = cal_days_in_month(CAL_GREGORIAN, $month, $year); // Número de dias del mes
+            $contador = 1; // contador para saber el número de dia que introducir
+            $semanas = ceil(($numDias + $numDay) / 7); // sacar el número de semanas que tiene el mes
             for ($i = 1; $i <= $semanas; $i++) {
                 echo "<tr>";
 
+                // NÚMERO DE DÍA
                 echo "<tr class='day'>";
                 if ($i == 1) {
-                    for ($j = 0; $j < $numDay; $j++) echo "<th class='num'>0</th>";
-                    for ($j = $numDay; $j < 7; $j++) {
-                        if ($contador > $numDias) echo "<th class='num'>0</th>";
-                        else echo "<th class='num'>$contador</th>";
+                    for ($j = 0; $j < $numDay; $j++) echo "<th class='num hidden'>0</th>"; // ocultar los dias del mes pasado
+                    for ($j = $numDay; $j < 7; $j++) { // mostrar los dias del resto de la semana
+                        echo "<th class='num'>$contador</th>";
                         $contador++;
                     }
                 } else {
-                    for ($j = 0; $j < 7; $j++) {
-                        if ($contador > $numDias) echo "<th class='num'>0</th>";
+                    for ($j = 0; $j < 7; $j++) { // mostrar los dias de la semana
+                        if ($contador > $numDias) echo "<th class='num hidden'>0</th>";
                         else echo "<th class='num'>$contador</th>";
                         $contador++;
                     }
                 }
                 echo "</tr>";
 
+                // INPUT TEXT BOX
                 echo "<tr>";
-                for ($j = 0; $j < 7; $j++)
-                    echo "<td class='value'><input type='text' placeholder='Enter text...' /></td>";
+                if ($i == 1) {
+                    for ($j = 0; $j < $numDay; $j++) // ocultar los dias del mes pasado
+                        echo "<td class='value hidden'><input type='text' placeholder='Enter text...' /></td>";
+                    for ($j = $numDay; $j < 7; $j++) // mostrar los dias del resto de la semana
+                        echo "<td class='value'><input type='text' placeholder='Enter text...' /></td>";
+                } else {
+                    $contador = $contador - 7; // restar 7 al contador para agregar el input al día
+                    for ($j = 0; $j < 7; $j++) { // mostrar los dias de la semana
+                        if ($contador > $numDias) echo "<td class='value hidden'><input type='text' placeholder='Enter text...' /></td>";
+                        else echo "<td class='value'><input type='text' placeholder='Enter text...' /></td>";
+                        $contador++;
+                    }
+                }
                 echo "</tr>";
 
                 echo "</tr>";
